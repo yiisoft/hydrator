531bcd90d7b0292c7e502c6ffae798f95a678003
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-24 22:01:33 +0300

    Improve `Map` attribute tests

--- tests/Support/Classes/MapNonStrictClass.php
+++ tests/Support/Classes/MapNonStrictClass.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Hydrator\Tests\Support\Classes;
 
 use Yiisoft\Hydrator\Attribute\Data\Map;
-use Yiisoft\Hydrator\Tests\Support\CustomData;
 
-#[CustomData(['a' => 1, 'y' => 2, 'c' => 3])]
 #[Map(['b' => 'y'], false)]
 final class MapNonStrictClass
 {
