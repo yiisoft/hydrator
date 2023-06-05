fa600e1a4871a79809f6679dffdf86a66d14eeaa
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-06-05 15:03:02 +0300

    Improve configuration

--- config/di.php
+++ config/di.php
@@ -2,9 +2,17 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
+use Yiisoft\Definitions\Reference;
 use Yiisoft\Hydrator\Hydrator;
 use Yiisoft\Hydrator\HydratorInterface;
 
 return [
-    HydratorInterface::class => Hydrator::class,
+    HydratorInterface::class => [
+        'class' => Hydrator::class,
+        '__construct()' => [
+            'attributeResolverContainer' => Reference::to(ContainerInterface::class),
+            'injectorContainer' => Reference::to(ContainerInterface::class),
+        ],
+    ],
 ];
