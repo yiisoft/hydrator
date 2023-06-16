db439898910dffc9bd5197ea49dc53d3ba3a9d52
Dmitrii Derepko <xepozz@list.ru>
2023-06-16 21:45:35 +0300

    Remove hydrator definition

--- config/di.php
+++ config/di.php
@@ -2,16 +2,9 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Definitions\Reference;
 use Yiisoft\Hydrator\Hydrator;
 use Yiisoft\Hydrator\HydratorInterface;
-use Yiisoft\Hydrator\TypeCaster\NoTypeCaster;
 
 return [
     HydratorInterface::class => Hydrator::class,
-    Hydrator::class => [
-        '__construct()' => [
-            'typeCaster' => Reference::to(NoTypeCaster::class),
-        ],
-    ],
 ];
