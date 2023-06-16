666eb47d38f84cc26d54eee1880a53512c9d6773
Dmitrii Derepko <xepozz@list.ru>
2023-06-16 20:46:25 +0300

    Remove nesting

--- src/TypeCaster/SimpleTypeCaster.php
+++ src/TypeCaster/SimpleTypeCaster.php
@@ -10,8 +10,8 @@ use ReflectionType;
 use ReflectionUnionType;
 use Stringable;
 use Yiisoft\Hydrator\HydratorInterface;
-use Yiisoft\Hydrator\TypeCasterInterface;
 use Yiisoft\Hydrator\Result;
+use Yiisoft\Hydrator\TypeCasterInterface;
 use Yiisoft\Strings\NumericHelper;
 
 use function is_array;
@@ -44,106 +44,110 @@ final class SimpleTypeCaster implements TypeCasterInterface
 
     public function cast(mixed $value, ?ReflectionType $type): Result
     {
+        if ($type === null) {
+            return Result::success($value);
+        }
+
+        if (!$type instanceof ReflectionNamedType && !$type instanceof ReflectionUnionType) {
+            return Result::fail();
+        }
+
         if ($type instanceof ReflectionNamedType) {
             $types = [$type];
-        } elseif ($type instanceof ReflectionUnionType) {
+        } else {
             $types = array_filter(
                 $type->getTypes(),
                 static fn(mixed $type) => $type instanceof ReflectionNamedType,
             );
-        } elseif ($type === null) {
-            return Result::success($value);
-        } else {
-            return Result::fail();
         }
 
         foreach ($types as $t) {
             if (null === $value && $t->allowsNull()) {
                 return Result::success(null);
             }
-            if ($t->isBuiltin()) {
-                switch ($t->getName()) {
-                    case 'string':
-                        if (is_string($value)) {
-                            return Result::success($value);
-                        }
-                        break;
+            if (!$t->isBuiltin()) {
+                continue;
+            }
+            switch ($t->getName()) {
+                case 'string':
+                    if (is_string($value)) {
+                        return Result::success($value);
+                    }
+                    break;
 
-                    case 'int':
-                        if (is_int($value)) {
-                            return Result::success($value);
-                        }
-                        break;
+                case 'int':
+                    if (is_int($value)) {
+                        return Result::success($value);
+                    }
+                    break;
 
-                    case 'float':
-                        if (is_float($value)) {
-                            return Result::success($value);
-                        }
-                        break;
+                case 'float':
+                    if (is_float($value)) {
+                        return Result::success($value);
+                    }
+                    break;
 
-                    case 'bool':
-                        if (is_bool($value)) {
-                            return Result::success($value);
-                        }
-                        break;
+                case 'bool':
+                    if (is_bool($value)) {
+                        return Result::success($value);
+                    }
+                    break;
 
-                    case 'array':
-                        if (is_array($value)) {
-                            return Result::success($value);
-                        }
-                        break;
-                }
+                case 'array':
+                    if (is_array($value)) {
+                        return Result::success($value);
+                    }
+                    break;
             }
         }
 
         foreach ($types as $t) {
-            if ($t->isBuiltin()) {
-                switch ($t->getName()) {
-                    case 'string':
-                        if (is_scalar($value) || null === $value || $value instanceof Stringable) {
-                            return Result::success((string) $value);
-                        }
-                        break;
-
-                    case 'int':
-                        if (is_bool($value) || is_float($value) || null === $value) {
-                            return Result::success((int) $value);
-                        }
-                        if ($value instanceof Stringable || is_string($value)) {
-                            return Result::success((int) NumericHelper::normalize((string) $value));
-                        }
-                        break;
-
-                    case 'float':
-                        if (is_int($value) || is_bool($value) || null === $value) {
-                            return Result::success((float) $value);
-                        }
-                        if ($value instanceof Stringable || is_string($value)) {
-                            return Result::success((float) NumericHelper::normalize((string) $value));
-                        }
-                        break;
-
-                    case 'bool':
-                        if (is_scalar($value) || null === $value || is_array($value) || is_object($value)) {
-                            return Result::success((bool) $value);
-                        }
-                        break;
+            if (!$t->isBuiltin()) {
+                $class = $t->getName();
+                if (is_object($value)) {
+                    if (is_a($value, $class)) {
+                        return Result::success($value);
+                    }
+                } elseif (is_array($value) && $this->hydrator !== null) {
+                    $reflection = new ReflectionClass($class);
+                    if ($reflection->isInstantiable()) {
+                        return Result::success(
+                            $this->hydrator->create($class, $value)
+                        );
+                    }
                 }
                 continue;
             }
+            switch ($t->getName()) {
+                case 'string':
+                    if (is_scalar($value) || null === $value || $value instanceof Stringable) {
+                        return Result::success((string) $value);
+                    }
+                    break;
 
-            $class = $t->getName();
-            if (is_object($value)) {
-                if (is_a($value, $class)) {
-                    return Result::success($value);
-                }
-            } elseif (is_array($value) && $this->hydrator !== null) {
-                $reflection = new ReflectionClass($class);
-                if ($reflection->isInstantiable()) {
-                    return Result::success(
-                        $this->hydrator->create($class, $value)
-                    );
-                }
+                case 'int':
+                    if (is_bool($value) || is_float($value) || null === $value) {
+                        return Result::success((int) $value);
+                    }
+                    if ($value instanceof Stringable || is_string($value)) {
+                        return Result::success((int) NumericHelper::normalize((string) $value));
+                    }
+                    break;
+
+                case 'float':
+                    if (is_int($value) || is_bool($value) || null === $value) {
+                        return Result::success((float) $value);
+                    }
+                    if ($value instanceof Stringable || is_string($value)) {
+                        return Result::success((float) NumericHelper::normalize((string) $value));
+                    }
+                    break;
+
+                case 'bool':
+                    if (is_scalar($value) || null === $value || is_array($value) || is_object($value)) {
+                        return Result::success((bool) $value);
+                    }
+                    break;
             }
         }
 
