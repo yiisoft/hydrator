664d585dc989956309d2ebd026651bdf7f693a70
Dmitrii Derepko <xepozz@list.ru>
2023-09-13 09:26:29 +0300

    Remove annotation

--- src/ObjectFactory/ObjectFactoryInterface.php
+++ src/ObjectFactory/ObjectFactoryInterface.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Hydrator\ObjectFactory;
 
-use Psr\Container\NotFoundExceptionInterface;
 use ReflectionClass;
-
 interface ObjectFactoryInterface
 {
     /**
      * @psalm-template T of object
      * @psalm-param ReflectionClass<T> $reflectionClass
      * @psalm-return T
-     * @throws NotFoundExceptionInterface
      */
     public function create(ReflectionClass $reflectionClass, array $constructorArguments): object;
 }
