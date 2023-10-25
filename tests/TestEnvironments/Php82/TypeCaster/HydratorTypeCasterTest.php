4662c5555637c44ac2f997bd7d8e09140a495204
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-10-25 20:53:08 +0300

    add tests

---
+++ tests/TestEnvironments/Php82/TypeCaster/HydratorTypeCasterTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeCaster;
+
+use Closure;
+use Countable;
+use PHPUnit\Framework\TestCase;
+use ReflectionFunction;
+use Stringable;
+use Yiisoft\Hydrator\Hydrator;
+use Yiisoft\Hydrator\Result;
+use Yiisoft\Hydrator\Tests\Support\StringableObject;
+use Yiisoft\Hydrator\TypeCaster\HydratorTypeCaster;
+use Yiisoft\Hydrator\TypeCaster\TypeCastContext;
+
+final class HydratorTypeCasterTest extends TestCase
+{
+    public function dataBase(): array
+    {
+        return [
+            'array to intersection type' => [
+                Result::fail(),
+                ['string' => 'hello'],
+                $this->createContext(static fn(Stringable&Countable $object) => null),
+            ],
+            'array to union type with intersection type' => [
+                Result::success(new StringableObject('hello')),
+                ['string' => 'hello'],
+                $this->createContext(static fn(StringableObject|(Stringable&Countable) $object) => null),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataBase
+     */
+    public function testBase(Result $expected, mixed $value, TypeCastContext $context): void
+    {
+        $typeCaster = new HydratorTypeCaster();
+
+        $result = $typeCaster->cast($value, $context);
+
+        $this->assertSame($expected->isResolved(), $result->isResolved());
+        $this->assertEquals($expected->getValue(), $result->getValue());
+    }
+
+    private function createContext(Closure $fn): TypeCastContext
+    {
+        return new TypeCastContext(
+            new Hydrator(),
+            (new ReflectionFunction($fn))->getParameters()[0],
+        );
+    }
+}
