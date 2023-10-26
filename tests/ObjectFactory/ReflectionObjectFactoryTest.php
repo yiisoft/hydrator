b86be162eb441a599107518c35df9577a9f8d63f
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-10-26 15:16:01 +0300

    ReflectionObjectFactory

---
+++ tests/ObjectFactory/ReflectionObjectFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ObjectFactory;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use Yiisoft\Hydrator\Exception\WrongConstructorArgumentsCountException;
+use Yiisoft\Hydrator\ObjectFactory\ReflectionObjectFactory;
+use Yiisoft\Hydrator\Tests\Support\StringableObject;
+
+final class ReflectionObjectFactoryTest extends TestCase
+{
+    public function testWrongConstructorArgumentsCount(): void
+    {
+        $factory = new ReflectionObjectFactory();
+        $reflection = new ReflectionClass(StringableObject::class);
+
+        $this->expectException(WrongConstructorArgumentsCountException::class);
+        $this->expectExceptionMessage(
+            'Class "' . StringableObject::class . '" cannot be instantiated because it has 1 required parameters in constructor, but passed only 0.'
+        );
+        $factory->create($reflection, []);
+    }
+}
