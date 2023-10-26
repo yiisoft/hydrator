4f80d3bcc64e60ea925fdb9e8ffacad62b152213
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-10-26 15:02:33 +0300

    PhpNativeTypeCaster

---
+++ tests/TypeCaster/PhpNativeTypeCasterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeCaster;
+
+use Closure;
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Hydrator\Result;
+use Yiisoft\Hydrator\Tests\Support\TestHelper;
+use Yiisoft\Hydrator\TypeCaster\PhpNativeTypeCaster;
+
+final class PhpNativeTypeCasterTest extends TestCase
+{
+    public function dataBase(): array
+    {
+        return [
+            'string to int' => [
+                Result::success(42),
+                '42',
+                static fn(int $a) => null
+            ],
+            'string to float' => [
+                Result::success(42.52),
+                '42.52',
+                static fn(float $a) => null
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataBase
+     */
+    public function testBase(Result $expected, mixed $value, Closure $closure): void
+    {
+        $typeCaster = new PhpNativeTypeCaster();
+        $context = TestHelper::createTypeCastContext($closure);
+
+        $result = $typeCaster->cast($value, $context);
+
+        $this->assertSame($expected->isResolved(), $result->isResolved());
+        $this->assertSame($expected->getValue(), $result->getValue());
+    }
+}
