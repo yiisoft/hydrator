440ec55ea0a6f50028997a9e90a9e7bab5e7d782
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-12-27 23:22:20 +0300

    Add test for readonly class

---
+++ tests/TestEnvironments/Php82/Support/ReadonlyClass.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Hydrator\Tests\TestEnvironments\Php82\Support;
+
+final readonly class ReadonlyClass
+{
+    public function __construct(
+        public string $name,
+        public int $age,
+    )
+    {
+    }
+}
