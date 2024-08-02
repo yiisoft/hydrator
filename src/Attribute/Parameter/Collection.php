80b6d416b01baffe04beadaf2542ceea1d6d8b97
Alexey Rogachev <arogachev90@gmail.com>
2024-08-02 13:02:55 +0500

    Add PHPDoc [skip ci]

--- src/Attribute/Parameter/Collection.php
+++ src/Attribute/Parameter/Collection.php
@@ -6,6 +6,10 @@ namespace Yiisoft\Hydrator\Attribute\Parameter;
 
 use Attribute;
 
+/**
+ * Converts the resolved value to array of instances of the class specified in {@see Collection::$className}.
+ * Non-resolved and invalid values are skipped.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER | Attribute::IS_REPEATABLE)]
 final class Collection implements ParameterAttributeInterface
 {
