4eba0b3029274e083429ca525805ed0755dc3aab
Alexey Rogachev <arogachev90@gmail.com>
2024-08-02 14:20:45 +0500

    More tests

--- tests/Attribute/Parameter/CollectionTest.php
+++ tests/Attribute/Parameter/CollectionTest.php
@@ -13,6 +13,7 @@ use Yiisoft\Hydrator\Attribute\Parameter\CollectionResolver;
 use Yiisoft\Hydrator\AttributeHandling\Exception\UnexpectedAttributeException;
 use Yiisoft\Hydrator\AttributeHandling\ParameterAttributeResolveContext;
 use Yiisoft\Hydrator\AttributeHandling\ResolverFactory\ContainerAttributeResolverFactory;
+use Yiisoft\Hydrator\DataInterface;
 use Yiisoft\Hydrator\Hydrator;
 use Yiisoft\Hydrator\Result;
 use Yiisoft\Hydrator\Tests\Support\Attribute\Counter;
@@ -23,6 +24,7 @@ use Yiisoft\Hydrator\Tests\Support\Classes\Chart\Coordinates;
 use Yiisoft\Hydrator\Tests\Support\Classes\Chart\Point;
 use Yiisoft\Hydrator\Tests\Support\Classes\CounterClass;
 use Yiisoft\Hydrator\Tests\Support\Classes\Post;
+use Yiisoft\Hydrator\Tests\Support\Classes\PostCategory;
 use Yiisoft\Hydrator\Tests\Support\TestHelper;
 use Yiisoft\Test\Support\Container\SimpleContainer;
 
@@ -49,50 +51,41 @@ final class CollectionTest extends TestCase
     public function testNotResolvedValue(): void
     {
         $hydrator = new Hydrator();
-        $object = new class () {
-            #[Collection(Chart::class)]
-            public array $charts = [];
-        };
+        $object = new PostCategory();
 
-        $hydrator->hydrate($object, ['chart' => []]);
-        $this->assertEmpty($object->charts);
+        $hydrator->hydrate($object, ['post' => []]);
+        $this->assertEmpty($object->getPosts());
     }
 
     public function testInvalidValue(): void
     {
         $hydrator = new Hydrator();
-        $object = new class () {
-            #[Collection(Chart::class)]
-            public array $charts = [];
-        };
+        $object = new PostCategory();
 
-        $hydrator->hydrate($object, ['charts' => new stdClass()]);
-        $this->assertEmpty($object->charts);
+        $hydrator->hydrate($object, ['posts' => new stdClass()]);
+        $this->assertEmpty($object->getPosts());
     }
 
     public function testInvalidValueItem(): void
     {
         $hydrator = new Hydrator();
-        $object = new class () {
-            #[Collection(Chart::class)]
-            public array $charts = [];
-        };
+        $object = new PostCategory();
 
         $hydrator->hydrate(
             $object,
             [
-                'charts' => [
+                'posts' => [
                     new stdClass(),
                 ],
             ],
         );
-        $this->assertEquals($object->charts, [new stdClass()]);
+        $this->assertEquals($object->getPosts(), [new stdClass()]);
     }
 
     public static function dataBase(): array
     {
         return [
-            [
+            'basic' => [
                 new Collection(Post::class),
                 [
                     ['name' => 'Post 1'],
@@ -103,11 +96,67 @@ final class CollectionTest extends TestCase
                     new Post(name: 'Post 2', description: 'Description for post 2'),
                 ],
             ],
+            'nested, one to one and one to many relations' => [
+                new Collection(Chart::class),
+                [
+                    [
+                        'points' => [
+                            ['coordinates' => ['x' => -11, 'y' => 11], 'rgb' => [-1, 256, 0]],
+                            ['coordinates' => ['x' => -12, 'y' => 12], 'rgb' => [0, -2, 257]],
+                        ],
+                    ],
+                    [
+                        'points' => [
+                            ['coordinates' => ['x' => -1, 'y' => 1], 'rgb' => [0, 0, 0]],
+                            ['coordinates' => ['x' => -2, 'y' => 2], 'rgb' => [255, 255, 255]],
+                        ],
+                    ],
+                    [
+                        'points' => [
+                            ['coordinates' => ['x' => -13, 'y' => 13], 'rgb' => [-3, 258, 0]],
+                            ['coordinates' => ['x' => -14, 'y' => 14], 'rgb' => [0, -4, 259]],
+                        ],
+                    ],
+                ],
+                [
+                    new Chart([
+                        new Point(new Coordinates(-11, 11), [-1, 256, 0]),
+                        new Point(new Coordinates(-12, 12), [0, -2, 257]),
+                    ]),
+                    new Chart([
+                        new Point(new Coordinates(-1, 1), [0, 0, 0]),
+                        new Point(new Coordinates(-2, 2), [255, 255, 255]),
+                    ]),
+                    new Chart([
+                        new Point(new Coordinates(-13, 13), [-3, 258, 0]),
+                        new Point(new Coordinates(-14, 14), [0, -4, 259]),
+                    ]),
+                ],
+            ],
+            'value item provided by class' => [
+                new Collection(Post::class),
+                [
+                    ['name' => 'Post 1'],
+                    new class () implements DataInterface
+                    {
+                        public function getValue(string $name): Result
+                        {
+                            $value = $name === 'name' ? 'Post 2' : 'Description for post 2';
+
+                            return Result::success($value);
+                        }
+                    }
+                ],
+                [
+                    new Post(name: 'Post 1'),
+                    new Post(name: 'Post 2', description: 'Description for post 2'),
+                ],
+            ],
         ];
     }
 
     #[DataProvider('dataBase')]
-    public function testBase(Collection $attribute, array $value, array $expectedValue): void
+    public function testBase(Collection $attribute, array $value, mixed $expectedValue): void
     {
         $resolver = new CollectionResolver();
         $context = new ParameterAttributeResolveContext(
