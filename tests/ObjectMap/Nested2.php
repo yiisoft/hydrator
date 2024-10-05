589eabafd09673edecbd1c11ae3c4c91c7b3fad0
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-10-05 14:34:19 +0300

    fix test

--- tests/ObjectMap/Nested2.php
+++ tests/ObjectMap/Nested2.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace Yiisoft\Hydrator\Tests\ObjectMap;
 
-use Yiisoft\Hydrator\Attribute\Parameter\Data;
-
 final class Nested2
 {
     public string $var1 = '';
-    #[Data('var2')]
     public string $var2 = '';
 }
