3917daf6dbd062517649e6aff36053a2b42e286c
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-10-07 15:50:28 +0300

    Update src/ObjectMap.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/ObjectMap.php
+++ src/ObjectMap.php
@@ -7,7 +7,7 @@ namespace Yiisoft\Hydrator;
 use function array_key_exists;
 
 /**
- * Class provides a mapping of object property names to keys in the data array.
+ * Provides a mapping of object property names to keys in the data array.
  *
  * @psalm-import-type MapType from ArrayData
  */
